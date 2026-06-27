f03f5012d091202bb30a0d3da82995be14664b78
iqtester <1835ww@gmail.com>
2026-06-27 15:01:35 -0400

    search bar 1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: "XiaoQMi's Wiki",
-  tagline: 'All In One Wiki for Everyone',
+  tagline: 'All-in-One Wiki for Everyone',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -139,6 +139,41 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+    },
+    metadata: [
+      {
+        name: 'algolia-site-verification',
+        content: '73FFEDDD2608538D',
+      },
+    ],
+    algolia: {
+      appId: 'KXGC5V582F',
+      apiKey: 'c93205c7effd9f6cd9d50c77f8195fa2',
+      indexName: 'XiaoCrawler',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/',
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+
+      // Optional: whether you want to use the new Ask AI feature (undefined by default)
+      askAi: 'YOUR_ALGOLIA_ASK_AI_ASSISTANT_ID',
     },
   } satisfies Preset.ThemeConfig,
 
